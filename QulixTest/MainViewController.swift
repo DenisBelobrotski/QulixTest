754eb45865df8f6977f765a7ff5b814848d29ca4
Denis Belobrotski <denisbelobrotski@Deniss-Mac.local>
2018-02-01 10:46:54 -0800

    Add UICollectionView

--- QulixTest/MainViewController.swift
+++ QulixTest/MainViewController.swift
@@ -8,16 +8,65 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    var collectionView: UICollectionView?
+    let cellId = "GifCell"
+    let cellSpacing:CGFloat = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "Trendings"
+//        self.navigationController?.isNavigationBarHidden = true
+        
+        collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout())
+        collectionView?.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView!)
+        
+        collectionView?.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView?.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView?.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        collectionView?.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        collectionView?.backgroundColor = UIColor.white
+        
+        let collectionViewFlowLayout = UICollectionViewFlowLayout()
+        collectionView?.setCollectionViewLayout(collectionViewFlowLayout, animated: true)
+        collectionViewFlowLayout.scrollDirection = UICollectionViewScrollDirection.vertical
+        collectionViewFlowLayout.sectionInset = UIEdgeInsets(top: 0, left: cellSpacing, bottom: 0, right: cellSpacing)
+//        collectionViewFlowLayout.minimumLineSpacing = cellSpacing
+//        collectionViewFlowLayout.minimumInteritemSpacing = cellSpacing
+        
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = UIColor.orange
+        return cell
+    }
+    
+    //UICollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: 100, height: 100)
+        let width = (UIScreen.main.bounds.size.width - 3 * cellSpacing) / 2
+        let height = (UIScreen.main.bounds.size.height - 4 * cellSpacing) / 3
+        return CGSize(width: width, height: height)
     }
 
 
