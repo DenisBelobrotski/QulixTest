176f229b4dcd6590bc509c2b094a5efe48a5e666
Denis Belobrotski <denisbelobrotski@Deniss-Mac.local>
2018-02-01 01:51:34 -0800

    Initial commit

---
+++ QulixTest/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  QulixTest
+//
+//  Created by Denis Belobrotski on 31.01.2018.
+//  Copyright © 2018 Denis Belobrotski. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
